2604c9c6a89459b4e126de9d67a74f63c6bc919f
dialog.txt@gmail.com <wa6k5l9NIM>
2017-04-09 19:33:09 +0700

    works

--- main.cpp
+++ main.cpp
@@ -1,31 +1,32 @@
+/**
+ * Разработать функцию с переменным числом параметров. Для извлечения параметров из списка использовать операцию преобразования типа указателя.
+ * Последовательность указателей на вещественные переменные, ограниченная NULL. Функция возвращает динамический  массив указателей на эти переменныe.
+ */
+
 #include <stdio.h>
 #include <cstdlib>
 #include <ctime>
 
-struct list {
+struct list { // list
     float val;
     list *next;
-
 };
 
-void show(list *head, int sizeList) {
-    list *cure = head;
-    //int *linkValue = (int*)malloc(sizeof(int));
-    float *linkValue[sizeList];
-    float **pp = linkValue;
-    int i=0;
-    while (cure != NULL) {
-        printf("%f ", cure->val);
-        linkValue[i] = (&cure->val);
-        cure = cure->next;
-        printf("%f\n", *pp[i]);
-        i++;
-    }
-    printf("\n");
-    for (int j = 0; j<sizeList;j++){
-        printf("%f\n", *linkValue[j]);
-    }
+// get index from list
+float **getArray(list *head, int sizeList, ...) { //function with a variable number of parameters :)
+    list *cure = head; // get index head
 
+    float **pp, *p;
+    pp = new float *[sizeList]; //indexes array
+    int i = 0;
+    for (i = 0; i < sizeList; i++) {
+        p = new float;
+        *p = cure->val;
+        pp[i] = p;
+        cure = cure->next;
+    }
+    pp[i] = NULL;
+    return pp;
 }
 
 float getValue() {
@@ -42,7 +43,6 @@ int getSizeList() {
     return sizeList;
 }
 
-
 list getList(list *head, int sizeList) {
     head = NULL;
     for (int i = 0; i < sizeList; i++) {
@@ -54,13 +54,18 @@ list getList(list *head, int sizeList) {
     return *head;
 }
 
+void printArr(int sizeList, float **pp) {
+    for (int j = 0; j < sizeList; j++) {
+        printf("Value[%d] = %.2f\t$\n", j, *pp[j]);
+    }
+}
+
 int main() {
     int sizeList = getSizeList();
     list *head;
-    *head = getList(head, sizeList);
-    show(head, sizeList);
-    //getLink(head, sizeList);
-
+    *head = getList(head, sizeList); //list
+    float **pp = getArray(head, sizeList); //idexes array
+    printArr(sizeList, pp);
 }
 
 
